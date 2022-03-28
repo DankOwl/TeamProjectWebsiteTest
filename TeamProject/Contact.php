fff2b5ea325ae98b7e16758f5824892e62dda943
DankOwl <47462905+DankOwl@users.noreply.github.com>
2022-03-28 15:47:44 +0100

    adding initial files

---
+++ TeamProject/Contact.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link href="Temp.css" rel="stylesheet" type="text/css" />
+    <header><h1>Cover Art Planet</h1></header>
+    <h2>Contact Us</h2>
+
+</head>
+<body>
+
+<div>
+    <nav>
+        <ul>
+            <li tabindex ="1"><a href="HomePage.php">Home</a></li>
+            <li tabindex ="2"><a href="LoginForm.html">Login</a></li>
+            <li tabindex ="3"><a href="AboutUs.html">About Us</a></li>
+            <li tabindex="4"><a href="Store.php">Store</a></li>
+            <li tabindex="5"><a class="activePage" href="Contact.php">Contact</a></li>
+            <li tabindex="6"><a href="FAQ.php">FAQ</a></li>
+        </ul>
+    </nav>
+</div>
+
+<p>Contact Us here</p>
+
+</body>
+</html>
